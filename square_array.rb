228ecb47b77aae20024513e59209cdd7753b974f
Andrew Bourgeois <andrew@olympicmedia.com>
2020-04-05 22:47:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,7 @@ def square_array(array)
   new_array = []
   # array.each{|element| element ** 2}
   array.each do |element|
-    element 
+    element = element ** 2
+    new_array << element
   end
 end
