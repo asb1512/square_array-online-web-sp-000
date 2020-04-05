17162293eaae7d8c6956f6a0cfd6cc05e5d8f6f4
Andrew Bourgeois <andrew@olympicmedia.com>
2020-04-05 22:53:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ require 'pry'
 def square_array(array)
   new_array = []
   # array.each{|element| element ** 2}
-  array.each do |element|
+  array.each! do |element|
     element ** 2
     binding.pry
     return new_array << element
